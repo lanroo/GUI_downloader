5545a730482bd656f87d964d3bdd0883ba0ea4c9
ylanna <yladacs@gmail.com>
2022-10-21 14:39:07 -0300

    link space and resize image

--- download.py
+++ download.py
@@ -1,6 +1,9 @@
 
+from os import link
+from queue import Empty
 from tkinter import *
 from tkinter import filedialog
+from turtle import width
 
 screen = Tk()
 title = screen.title('Youtube Download')
@@ -11,8 +14,20 @@ Canvas.pack()
 
 logo_img = PhotoImage(file='yt.png')
 
-Canvas.create_image(250, 80, image=logo_img)
+#resize image
 
+logo_img = logo_img.subsample(2, 2)
+
+Canvas.create_image(250,80, image=logo_img)
+
+
+#Link field
+link_field = Entry(screen, width=50)
+link_label = Label(screen, text="URL do video: ")
+
+#Add widgets to window
+Canvas.create_window(250, 170, window=link_label)
+Canvas.create_window(250, 220, window=link_field)
 
 
 screen.mainloop()
