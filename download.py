a459abaa6b0c47beb6f8af752042eb252791d2ba
ylanna <yladacs@gmail.com>
2022-10-21 14:22:26 -0300

    screen and logo by canvas

---
+++ download.py
@@ -0,0 +1,18 @@
+
+from tkinter import *
+from tkinter import filedialog
+
+screen = Tk()
+title = screen.title('Youtube Download')
+Canvas = Canvas(screen, width=500, height=500)
+Canvas.pack()
+
+# image Logo
+
+logo_img = PhotoImage(file='yt.png')
+
+
+
+
+
+screen.mainloop()
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Requirements 
+
+### 1 - Install Tkinter:
+
+For Debian-based Linux:
+
+`sudo apt-get install python-tk`
+
+others: 
+
+https://www.tutorialspoint.com/how-to-install-tkinter-in-python
+
+
