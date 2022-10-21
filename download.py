98d92e30bd4730e33950e1905eaaa7728c4d7275
ylanna <yladacs@gmail.com>
2022-10-21 14:47:26 -0300

    path, field and window conf

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 
+from curses import window
 from os import link
 from queue import Empty
 from tkinter import *
@@ -23,11 +24,26 @@ Canvas.create_image(250,80, image=logo_img)
 
 #Link field
 link_field = Entry(screen, width=50)
-link_label = Label(screen, text="URL do video: ")
+link_label = Label(screen, text="URL do video: ", font=('Arial', 15))
+
+# Select Path for saving the file
+
+path_label = Label(screen, text="Selecione a pasta de destino", font=('Arial', 15))
+select_btn = Button(screen, text="Selecionar")
+
+# Add to window
+
+Canvas.create_window(250, 280, window=path_label)
+Canvas.create_window(250, 330, window=select_btn)
+
 
 #Add widgets to window
 Canvas.create_window(250, 170, window=link_label)
 Canvas.create_window(250, 220, window=link_field)
+
+#Downloads btns
+
+download_btn = Button(screen, text="Download Video")
 
 
 screen.mainloop()
